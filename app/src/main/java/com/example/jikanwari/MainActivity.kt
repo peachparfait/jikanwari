bd4a8967dcee7b3adb165ed3d94a0c7e36ec3fed
peachparfait <peachparfait10@gmail.com>
2020-05-15 00:00:19 +0900

    SQLite導入準備

--- app/src/main/java/com/example/jikanwari/MainActivity.kt
+++ app/src/main/java/com/example/jikanwari/MainActivity.kt
@@ -1,12 +1,83 @@
 package com.example.jikanwari
-
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
+object FeedReaderContract {
+    // Table contents are grouped together in an anonymous object.
+    object FeedEntry : BaseColumns {
+        const val TABLE_NAME = "entry"
+        const val COLUMN_NAME_TITLE = "title"
+        const val COLUMN_NAME_SUBTITLE = "subtitle"
+    }
+}
+
+private const val SQL_CREATE_ENTRIES =
+    "CREATE TABLE ${FeedReaderContract.FeedEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE} TEXT," +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE} TEXT)"
+
+private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${FeedReaderContract.FeedEntry.TABLE_NAME}"
+class FeedReaderDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_ENTRIES)
+    }
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db, oldVersion, newVersion)
+    }
+    companion object {
+        // If you change the database schema, you must increment the database version.
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "FeedReader.db"
+    }
+}
+
 class MainActivity : AppCompatActivity(){
+    val dbHelper = FeedReaderDbHelper(this)
+    // Gets the data repository in write mode
+    val db = dbHelper.writableDatabase
+
+    // Create a new map of values, where column names are the keys
+    val values = ContentValues().apply {
+        put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, "title")
+        put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE, "subtitle")
+    }
+    // Insert the new row, returning the primary key value of the new row
+    val newRowId = db?.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values)
+    val readdb = dbHelper.readableDatabase
+    // Define a projection that specifies which columns from the database
+    // you will actually use after this query.
+    val projection = arrayOf(BaseColumns._ID, FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE)
+    // Filter results WHERE "title" = 'My Title'
+    val selection = "${FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE} = ?"
+    val selectionArgs = arrayOf("My Title")
+    // How you want the results sorted in the resulting Cursor
+    val sortOrder = "${FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE} DESC"
+    val cursor = readdb.query(
+        FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+        projection,             // The array of columns to return (pass null to get all)
+        selection,              // The columns for the WHERE clause
+        selectionArgs,          // The values for the WHERE clause
+        null,                   // don't group the rows
+        null,                   // don't filter by row groups
+        sortOrder               // The sort order
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 }
+
+
